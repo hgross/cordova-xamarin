079c182ce37b1b8f691565dfcbd14fecbdb67479
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2016-05-24 11:43:41 +0200

    Added parser_file property and fixed copy

--- scripts/modifyPlatforms.js
+++ scripts/modifyPlatforms.js
@@ -5,11 +5,13 @@ var PLATFORMS_JSON_PATH = path.resolve(__dirname + "/../node_modules/cordova-lib
     
 var platformsConfig = require(PLATFORMS_JSON_PATH);  
 platformsConfig["xamarin-ios"] = {
+    "parser_file": "../cordova/metadata/xamarin-android_parser",
     "url": "https://bitbucket.org/henna/cordova-xamarin-ios",
     "version": "~1.0.0",
     "deprecated": false
 }; 
 platformsConfig["xamarin-android"] = {
+    "parser_file": "../cordova/metadata/xamarin-android_parser",
     "url": "https://bitbucket.org/henna/cordova-xamarin-android",
     "version": "~1.0.0",
     "deprecated": false
@@ -24,8 +26,8 @@ fs.writeFile(PLATFORMS_JSON_PATH, JSON.stringify(platformsConfig, null, 2), func
 }); 
 
 function copyMetadata() {
-    var METADATA_ORIGIN_PATH = path.resolve(__dirname + "cordova-lib");
-    var METADATA_PATH = path.resolve(__dirname, + "/../node_modules/cordova-lib");
+    var METADATA_ORIGIN_PATH = path.resolve(__dirname + "/cordova-lib/*");
+    var METADATA_PATH = path.resolve(__dirname + "/../node_modules/cordova-lib");
     
     console.log("Copying metdata from " + METADATA_ORIGIN_PATH + " to " + METADATA_PATH);
     shell.cp("-R", METADATA_ORIGIN_PATH, METADATA_PATH);
