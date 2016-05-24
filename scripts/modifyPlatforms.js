03c3eba73c6513fe7fd3e0ffca1ad12e94b240f5
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2016-05-24 10:44:01 +0200

    Added parser file in metadata and helper function in postinstall script

--- scripts/modifyPlatforms.js
+++ scripts/modifyPlatforms.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require("path");
+var shell = require("shelljs");
 var PLATFORMS_JSON_PATH = path.resolve(__dirname + "/../node_modules/cordova-lib/src/platforms/platformsConfig.json");
     
 var platformsConfig = require(PLATFORMS_JSON_PATH);  
@@ -21,3 +22,13 @@ fs.writeFile(PLATFORMS_JSON_PATH, JSON.stringify(platformsConfig, null, 2), func
 
     console.log("Modified the platformsConfig.json to enable xamarin ios and android platforms.");
 }); 
+
+function copyMetadata() {
+    var METADATA_ORIGIN_PATH = path.resolve(__dirname + "cordova-lib");
+    var METADATA_PATH = path.resolve(__dirname, + "/../node_modules/cordova-lib");
+    
+    console.log("Copying metdata from " + METADATA_ORIGIN_PATH + " to " + METADATA_PATH);
+    shell.cp("-R", METADATA_ORIGIN_PATH, METADATA_PATH);
+}
+
+copyMetadata();
